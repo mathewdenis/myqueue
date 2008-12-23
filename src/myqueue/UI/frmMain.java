de5ffbf1fc3d7298768a414260177a05cefc8977
nsiatras <nsiatras@8aa3d242-df54-0410-a516-3d5b7e73011a>
2008-12-23 19:34:50 +0000

    About form implemented

--- src/myqueue/UI/frmMain.java
+++ src/myqueue/UI/frmMain.java
@@ -49,7 +49,7 @@ public class frmMain extends javax.swing.JFrame
         {
             System.err.println(ex.getMessage());
         }
-        initComponents();       
+        initComponents();
 
         // Load myQueueUI.properties.
         try
@@ -130,6 +130,8 @@ public class frmMain extends javax.swing.JFrame
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         jPopupMenu1.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
             public void popupMenuCanceled(javax.swing.event.PopupMenuEvent evt) {
@@ -254,6 +256,18 @@ public class frmMain extends javax.swing.JFrame
         jMenu1.add(jMenuItem2);
 
         jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Help");
+
+        jMenuItem3.setText("About");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
+
+        jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
 
@@ -493,6 +507,11 @@ public class frmMain extends javax.swing.JFrame
         }
     }//GEN-LAST:event_menuItemShowServerActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        frmAbout frm = new frmAbout();
+        frm.setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     private void HideForm()
     {
         menuItemShowServer.setLabel("Show");
@@ -582,13 +601,13 @@ public class frmMain extends javax.swing.JFrame
             }
         }
     }
-
-    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItemClear;
     private javax.swing.JMenuItem jMenuItemDelete;
     private javax.swing.JMenuItem jMenuItemEdit;
