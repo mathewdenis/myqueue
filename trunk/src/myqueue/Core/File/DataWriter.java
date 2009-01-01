85f4ba79e7758d9d8c4cd56680f3d9a315d057d7
nsiatras <nsiatras@8aa3d242-df54-0410-a516-3d5b7e73011a>
2009-01-01 17:11:50 +0000

    Copyright year changed

--- trunk/src/myqueue/Core/File/DataWriter.java
+++ trunk/src/myqueue/Core/File/DataWriter.java
@@ -1,5 +1,5 @@
 /* myQueue
- * Copyright (C) 2008 Nikos Siatras
+ * Copyright (C) 2008-2009 Nikos Siatras
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
